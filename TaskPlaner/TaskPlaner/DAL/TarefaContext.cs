0012eb3c52418383fb16aaa705016868c63be958
rfnpinto <ricardo995.pinto@gmail.com>
2014-10-09 19:45:33 +0100

    tarefa context

---
+++ TaskPlaner/TaskPlaner/DAL/TarefaContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ContosoUniversity.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace TaskPlaner.DAL
+{
+    public class TarefaContext : DbContext
+    {
+        public TarefaContext():base("TarefaCoontext")
+    }
+}
