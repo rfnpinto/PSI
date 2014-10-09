2bad2b97d02423daca6f8c7c0169c40cdf9fd2a0
rfnpinto <ricardo995.pinto@gmail.com>
2014-10-09 19:16:58 +0100

    Modelo Tarefa

---
+++ TaskPlaner/TaskPlaner/Models/Tarefa.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace TaskPlaner.Models
+{
+    public abstract class Tarefa
+    {
+        public int ID { get; set; }
+
+        [Required]
+        public DateTime Data { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "O Titulo deve ter menos de 50 caracteres;")]
+        public string Titulo { get; set; }
+
+        [Required]
+        [StringLength(250, ErrorMessage = "O tamanho maximo da nota é 250 caracteres;")]
+        public int Nota { get; set; }
+
+        [Required]
+        public bool Lido { get; set; } 
+    }
+}
