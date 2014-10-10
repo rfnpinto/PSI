20f7c96247747233b3f4a6025fa1767ccabee9f4
JoseMariaAndrade <jmariaandrade131@gmail.com>
2014-10-10 01:19:02 +0100

    Controller

---
+++ TaskPlaner/TaskPlaner/Controllers/TarefaController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TaskPlaner.Models;
+using TaskPlaner.DAL;
+
+namespace TaskPlaner.Controllers
+{
+    public class TarefaController : Controller
+    {
+        private TarefaContext db = new TarefaContext();
+
+        // GET: /Tarefa/
+        public ActionResult Index()
+        {
+            return View(db.Tarefas.ToList());
+        }
+
+        // GET: /Tarefa/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tarefa tarefa = db.Tarefas.Find(id);
+            if (tarefa == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tarefa);
+        }
+
+        // GET: /Tarefa/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /Tarefa/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="ID,Data,Titulo,Nota,Lido")] Tarefa tarefa)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Tarefas.Add(tarefa);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(tarefa);
+        }
+
+        // GET: /Tarefa/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tarefa tarefa = db.Tarefas.Find(id);
+            if (tarefa == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tarefa);
+        }
+
+        // POST: /Tarefa/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="ID,Data,Titulo,Nota,Lido")] Tarefa tarefa)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tarefa).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(tarefa);
+        }
+
+        // GET: /Tarefa/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tarefa tarefa = db.Tarefas.Find(id);
+            if (tarefa == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tarefa);
+        }
+
+        // POST: /Tarefa/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Tarefa tarefa = db.Tarefas.Find(id);
+            db.Tarefas.Remove(tarefa);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
